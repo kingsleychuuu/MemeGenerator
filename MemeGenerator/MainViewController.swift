0bccb8ee1c8f887d38510ccf74943ad3afe714a8
kingsleychuuu <kingsleychu0730@hotmail.com>
2020-05-16 05:05:36 -0400

    picker and actionsheet methods added

--- MemeGenerator/MainViewController.swift
+++ MemeGenerator/MainViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UINavigationControllerDelegate {
     
     let imageView = UIImageView()
 
@@ -18,11 +18,40 @@ class MainViewController: UIViewController {
     
     fileprivate func setupNavBar() {
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveButtonTapped))
     }
     
     @objc func addButtonTapped() {
+        let ac = UIAlertController(title: "Select Image Source", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Camera", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Gallery", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    @objc func saveButtonTapped() {
         
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let pickerViewController = UIImagePickerController()
+        pickerViewController.sourceType = action.title == "Camera" ? UIImagePickerController.SourceType.camera : UIImagePickerController.SourceType.photoLibrary
+        pickerViewController.allowsEditing = true
+        pickerViewController.delegate = self
+        present(pickerViewController, animated: true, completion: nil)
     }
 }
 
-extension
+extension MainViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+        
+        imageView.image = image
+    }
+}
